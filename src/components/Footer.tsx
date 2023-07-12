88a3b164c2194d7b7a77dad2f38b4944fe6f6fa7
abotstheboss <abolarinoluwatobistephen@gmail.com>
2023-07-12 12:40:02 +0100

    feat: contact me functionality

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,16 +3,39 @@ import SectionTitle from "./home/SectionTitle";
 import { motion } from "framer-motion";
 import { BsArrowRight } from "react-icons/bs";
 import { FormEvent, useState } from "react";
+import emailJs from "@emailjs/browser";
 
 function Footer() {
 	const [name, setName] = useState("");
 	const [email, setEmail] = useState("");
 	const [message, setMessage] = useState("");
+	const [isLoading, setIsLoading] = useState(false);
+	const [isSent, setIsSent] = useState(false);
+	const [isError, setIsError] = useState(false);
 
-	function onSubmit(e: FormEvent<HTMLFormElement>) {
+	async function onSubmit(e: FormEvent<HTMLFormElement>) {
 		e.preventDefault();
-
-		console.log(name, email, message);
+		setIsLoading(true);
+		try {
+			await emailJs.send(
+				import.meta.env.VITE_EMAILJS_SERVICE_ID,
+				import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+				{
+					from_name: name + " " + email,
+					message,
+				},
+				import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+			);
+			setIsSent(true);
+			setIsError(false);
+		} catch (error) {
+			setIsError(true);
+		} finally {
+			setEmail("");
+			setName("");
+			setMessage("");
+			setIsLoading(false);
+		}
 	}
 
 	return (
@@ -23,34 +46,41 @@ function Footer() {
 				<Top>
 					<Group>
 						<label htmlFor="name">name</label>
-						<input type="text" id="name" onChange={(e) => setName(e.target.value)} />
+						<input type="text" id="name" onChange={(e) => setName(e.target.value)} value={name} />
 					</Group>
 					<Group>
 						<label htmlFor="email">email</label>
-						<input type="email" id="email" onChange={(e) => setEmail(e.target.value)} />
+						<input type="email" id="email" onChange={(e) => setEmail(e.target.value)} value={email} />
 					</Group>
 				</Top>
 				<Group>
 					<label htmlFor="message">message</label>
-					<textarea id="message" onChange={(e) => setMessage(e.target.value)} />
+					<textarea id="message" onChange={(e) => setMessage(e.target.value)} value={message} />
 				</Group>
-				<Submit type="submit">
-					<span>send</span>
-					<motion.div
-						className="icon"
-						initial={{ x: 5 }}
-						animate={{ x: 0 }}
-						transition={{ repeat: Infinity, duration: 0.5, repeatType: "reverse", ease: "easeIn" }}
-					>
-						<BsArrowRight />
-					</motion.div>
+				<Submit type="submit" disabled={isSent}>
+					{!isLoading && !isSent && <span>send</span>}
+					{isLoading && <span>sending</span>}
+					{isSent && <span>sent!</span>}
+					{!isLoading && !isSent && (
+						<motion.div
+							className="icon"
+							initial={{ x: 5 }}
+							animate={{ x: 0 }}
+							transition={{ repeat: Infinity, duration: 0.5, repeatType: "reverse", ease: "easeIn" }}
+						>
+							<BsArrowRight />
+						</motion.div>
+					)}
 				</Submit>
+				{isError && <div>Oops. Something went wrong. Try again another time!</div>}
 			</Container>
 		</footer>
 	);
 }
 
 export default Footer;
+
+//service_n7kxlcf  Service id
 
 const Submit = styled.button`
 	margin-left: auto;
@@ -75,6 +105,11 @@ const Submit = styled.button`
 		background-size: 51% 3px;
 		background-position: 0% 100%, 100% 100%;
 		gap: 2rem;
+	}
+
+	&:disabled {
+		background: none;
+		color: #828282;
 	}
 
 	.icon {
